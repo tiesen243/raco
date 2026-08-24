efb56b9b0f456219bc7a6b06a4b67f95eafa3014
tiesen243 <ttien56906@gmail.com>
2026-08-24 15:33:30 +0700

    feat(ci): deploy

--- packages/alchemy/alchemy.run.ts
+++ packages/alchemy/alchemy.run.ts
@@ -1,15 +1,20 @@
 import Api from '@raco/api'
 import * as Alchemy from 'alchemy'
 import * as Cloudflare from 'alchemy/Cloudflare'
+import * as GitHub from 'alchemy/GitHub'
+import * as Output from 'alchemy/Output'
 import * as Effect from 'effect/Effect'
+import * as Layer from 'effect/Layer'
 
 export default Alchemy.Stack(
   'raco',
   {
-    providers: Cloudflare.providers(),
+    providers: Layer.merge(Cloudflare.providers(), GitHub.providers()),
     state: Cloudflare.state(),
   },
   Effect.gen(function* () {
+    const github = yield* GitHub.GitHubEnv
+
     const api = yield* Api
     const web = yield* Cloudflare.Website.Vite('web', {
       rootDir: '../../apps/web',
@@ -22,6 +27,24 @@ export default Alchemy.Stack(
         VITE_API_URL: api.url.as<string>(),
       },
     })
+
+    if (github?.pr)
+      yield* GitHub.Comment('preview', {
+        owner: github.owner,
+        repository: github.repository,
+        issueNumber: github.pr,
+        body: Output.interpolate`
+          ### Preview
+
+          - **API**: [${api.url}](${api.url})
+          - **Web**: [${web.url}](${web.url})
+
+          Built from commit ${github.sha.slice(0, 7)}
+
+          ---
+          _This comment updates automatically with each push._
+        `,
+      })
 
     return {
       api: api.url,
